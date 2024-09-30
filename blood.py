652bfb54d8327c317f8070fad17669ba1e1edb10
Sub_Per <118508299+nsubagya@users.noreply.github.com>
2024-09-30 01:55:23 -0400

    Update blood.py

--- blood.py
+++ blood.py
@@ -5,8 +5,10 @@ import streamlit as st
 from datetime import datetime
 from tabulate import tabulate
 
-# Display local image
+
+# Display image from URL
 st.image("https://github.com/nsubagya/Blood_transfusion_predict/blob/main/2uypz7k4.png", caption="Blood Donation Process", use_column_width=True)
+
 
 
 # Load the model from the file
