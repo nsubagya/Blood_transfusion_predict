ee0354b31ccacbab8ead98bbd11535ed5f222842
Sub_Per <118508299+nsubagya@users.noreply.github.com>
2024-09-30 01:52:28 -0400

    Update blood.py

--- blood.py
+++ blood.py
@@ -5,6 +5,10 @@ import streamlit as st
 from datetime import datetime
 from tabulate import tabulate
 
+# Display local image
+st.image("https://github.com/nsubagya/Blood_transfusion_predict/blob/main/2uypz7k4.png", caption="Blood Donation Process", use_column_width=True)
+
+
 # Load the model from the file
 model = joblib.load('decision_tree_model.pkl')
 
